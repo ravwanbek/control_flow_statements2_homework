ce7c95d46c77d4c4c524df30af81ed279f94e950
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-04 02:19:32 +0900

    1

--- if01.py
+++ if01.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    answer=''
+    if a>b and a>c:
+        answer=a
+    elif b>a and b>c:
+        answer=b
+    elif c>a and c>b:
+        answer=c 
+
+    return answer
+answer=main(3,4,5)
+print(answer)
